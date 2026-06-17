f9c5218ce87d4432833e196c8f3dd3187b0a4457
Kavya Sri <vkaviyasri19@gmail.com>
2026-06-17 14:27:47 +0530

    First React Project

---
+++ react-github-demo/src/App.jsx
@@ -0,0 +1,18 @@
+import "./App.css";
+
+function App() {
+  return (
+    <div className="container">
+      <h1>Student Portfolio</h1>
+
+      <div className="card">
+        <h2>Kavya Sri</h2>
+        <p>Frontend Developer</p>
+
+        <button>Contact Me</button>
+      </div>
+    </div>
+  );
+}
+
+export default App;
