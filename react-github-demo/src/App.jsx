92b7daf30a37e22976e4ef14cdf16d80f7b21ed7
Kavya Sri <vkaviyasri19@gmail.com>
2026-06-19 19:56:37 +0530

    Completed Portfolio Website

--- react-github-demo/src/App.jsx
+++ react-github-demo/src/App.jsx
@@ -8,6 +8,30 @@ function App() {
         <h2>Frontend Developer</h2>
         <p>Welcome to my Portfolio Website</p>
 
+        <hr />
+
+        <h3>About Me</h3>
+        <p>
+          I am a Frontend Developer passionate about building responsive
+          websites using HTML, CSS, JavaScript and React.
+        </p>
+
+        <h3>Skills</h3>
+        <ul>
+          <li>HTML</li>
+          <li>CSS</li>
+          <li>JavaScript</li>
+          <li>React</li>
+          <li>Git & GitHub</li>
+        </ul>
+
+        <h3>Projects</h3>
+        <ul>
+          <li>Portfolio Website</li>
+          <li>Calculator App</li>
+          <li>To-Do List</li>
+        </ul>
+
         <button>Contact Me</button>
       </div>
     </div>
